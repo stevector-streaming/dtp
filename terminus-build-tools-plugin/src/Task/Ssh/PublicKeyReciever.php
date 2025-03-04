26947d76c30b52f2bd2964dca3d6e72ad7ac1721
Steve Persch <steve.persch@pantheon.io>
2025-03-03 21:05:50 -0600

    redeleting

--- terminus-build-tools-plugin/src/Task/Ssh/PublicKeyReciever.php
+++
@@ -1,9 +0,0 @@
-<?php
-namespace Pantheon\TerminusBuildTools\Task\Ssh;
-
-use Pantheon\TerminusBuildTools\ServiceProviders\CIProviders\CIState;
-
-interface PublicKeyReciever
-{
-    public function addPublicKey(CIState $ci_env, $publicKey);
-}
