7d346f05fc6c03ac23daa4197c901acdae745b29
Steve Persch's alt account <102996065+stevector-streaming@users.noreply.github.com>
2025-03-05 14:20:31 -0600

    delete private key receiver (#26)
    Co-authored-by: Steve Persch <steve.persch@pantheon.io>

--- terminus-build-tools-plugin/src/Task/Ssh/PrivateKeyReciever.php
+++
@@ -1,9 +0,0 @@
-<?php
-namespace Pantheon\TerminusBuildTools\Task\Ssh;
-
-use Pantheon\TerminusBuildTools\ServiceProviders\CIProviders\CIState;
-
-interface PrivateKeyReciever
-{
-    public function addPrivateKey(CIState $ci_env, $privateKey);
-}
